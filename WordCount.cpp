aa253d2ed215ca6af596e79d926d351ef4e0db3c
Jakinda <qzhang423@ucsb.edu>
2025-03-09 17:02:08 -0700

    aaaaaa

--- WordCount.cpp
+++ WordCount.cpp
@@ -218,41 +218,36 @@ void WordCount::dumpWordsSortedByWord(std::ostream &out) const {
 		}
 	}
 	sort(words.begin(), words.end(), [](const pair<string, int> &a, const pair<string, int> &b) {
-		return a.first < b.first;  
+		return a.first > b.first;
 	});
 
 	for (const auto &entry : words) {
 		out << entry.first << "," << entry.second << "\n";
 	}
 }
-
 
 void WordCount::dumpWordsSortedByOccurence(std::ostream &out) const {
-	vector<pair<string, int>> words;
-	for (size_t i = 0; i < CAPACITY; i++) {
-		for (const auto &entry : table[i]) {
-			words.push_back(entry);
-		}
-	}
-	sort(words.begin(), words.end(), [](const pair<string, int> &a, const pair<string, int> &b) {
-		if (a.second != b.second) {
-			return a.second > b.second;  
-		}
-		return a.first < b.first; 
-	});
+    vector<pair<string, int>> words;
+    for (size_t i = 0; i < CAPACITY; i++) {
+        for (const auto &entry : table[i]) {
+            words.push_back(entry);
+        }
+    } 
+    sort(words.begin(), words.end(), [](const pair<string, int> &a, const pair<string, int> &b) {
+        if (a.second != b.second) {
+            return a.second > b.second; 
+        }
+        return a.first < b.first; 
+    });
 
-	for (const auto &entry : words) {
-		out << entry.first << "," << entry.second << "\n";
-	}
+    for (const auto &entry : words) {
+        out << entry.first << "," << entry.second << "\n";
+    }
 }
-
 void WordCount::addAllWords(std::string text) {
 	stringstream ss(text);
 	string word;
 	while (ss >> word) {
-		word = makeValidWord(word); 
-		if (!word.empty()) { 
-			incrWordCount(word);
-		}
+		incrWordCount(word);
 	}
 }
